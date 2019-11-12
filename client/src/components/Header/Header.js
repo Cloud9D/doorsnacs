ad287354189ee2296891bfc3d90389a383edf277
Connor <connordupuis760@gmail.com>
2019-11-12 16:53:14 -0500

    Revert "Landing page"

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,32 +1,31 @@
-
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import { Nav } from 'react-bootstrap';
-import { Form } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import './customNav.css';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
+const Header = () => {
+    return (
+        <div className='topnav'>
+            {/* Logo */}
+            <Link id="logo-link" to="/">
+                <img className="topnav-logo" src={ "/logo192.png" } alt="React logo" />
+            </Link>
 
-export default class Header extends React.Component{
-    render(){
-        return(
-            <Navbar sticky="top" className="customNav" expand="lg">
-                <Navbar.Brand style={{fontSize: "22px", fontWeight: "bold", paddingRight: "50px"}} className="test" href="#home">
-                    <img alt="a" src="/logo.svg" width="80" height="30" className="d-inline-block align-center"></img>
-                    {"  "}doorsnacs</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link style={{fontSize: "17px",paddingRight: "20px"}} href="#home">Home</Nav.Link>
-                        <Nav.Link style={{fontSize: "17px"}} href="#link">Popular</Nav.Link>
-                    </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search for Food" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
+            {/* Page Links */}
+            <div className="topnav-right">
+                <Link className="topnav-link" to='/Register'>Sign in</Link>
+                <Link className="topnav-link" to='/projects'>Projects</Link>
+                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://www.facebook.com/groups/ufosc/events/?source=4&action_history=null&filter=calendar">
+                    Events
+                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6"></i>
+                </a>
+                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://github.com/ufosc/club-resources">
+                    Resources
+                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6 right-4"></i>
+                </a>
+                <Link className="topnav-link" to="/about">About</Link>
+            </div>
+        </div>
+    )
 }
+
+export default Header;
