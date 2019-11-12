7bc64e993fd9b3a2ed1e37406eb08ac124d923db
WittySkates <connordupuis760@gmail.com>
2019-11-12 06:49:09 -0500

    Created visual landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,46 @@
-import React from 'react';
-import { Route, Switch, Redirect  } from 'react-router-dom';
-import Home from "./views/Home/Home"
-import NotFound from "./views/NotFound"
-import Header from "./components/Header/Header"
+import React, { Component } from 'react';
+import Header from './Components/Header/Header'
+import Cards from './Components/Cards/Cards'
+import AddressDelivery from './Components/AddressDelivery/AddressDelivery';
+import './App.css';
 
 
-const App = () => {
+
+function App() {
   return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/Home" component={Home} />
-        <Route exact path="/">
-          <Redirect to="/Home" />
-        </Route>
-        <Route component={NotFound}/>
-      </Switch>
+    <div className="App">
+        <Header />
+        <div className="App-body">
+            <div style={{backgroundColor:"#4b4a52"}}>
+                <div className="address">
+                    <AddressDelivery/>
+                </div>
+            </div>
+            <div className="TopUsers">
+                <div>
+                    <h1 style={{paddingLeft:"80px",paddingTop:"50px", color:"white"}}>Top Users in Your Area</h1>
+                </div>
+                <div  className="bootstrap-override-container">
+                    <div className="row">
+                        <div class="bootstrap-override-col"> <Cards /></div>
+                        <div class="bootstrap-override-col"> <Cards /></div>
+                        <div class="bootstrap-override-col"> <Cards /></div>
+                    </div>
+                </div>
+            </div>
+            <div className="TopFood">
+                <div>
+                    <h1 style={{paddingLeft:"80px",paddingTop:"50px", color:"white"}}>Popular Items Near You</h1>
+                </div>
+                <div  className="bootstrap-override-container">
+                    <div className="row">
+                        <div class="bootstrap-override-col"> <Cards /></div>
+                        <div class="bootstrap-override-col"> <Cards /></div>
+                        <div class="bootstrap-override-col"> <Cards /></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
