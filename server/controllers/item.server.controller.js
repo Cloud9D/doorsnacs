ed705b82edf19b4c6c440d15f6964a478fcbba65
Marco Munoz <marcom001@ufl.edu>
2019-11-14 16:36:09 -0500

    renamed controller

---
+++ server/controllers/item.server.controller.js
@@ -0,0 +1,18 @@
+var Item = require('../models/item.server.model.js')
+
+exports.create = function(req, res) {
+	var item = new Item(req.bod);
+	item.save(function(err){
+    		if(err){
+			console.log(err);
+			res.status(400).send(err);
+		}else{
+			res.json(item);
+			console.log(item);
+		}
+	});
+};
+
+exports.read = function(req, res) {
+	res.json(req.item);
+};
