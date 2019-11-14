c31048792f0953e3c3f60084da49c800b0a072e7
Marco Munoz <marcom001@ufl.edu>
2019-11-14 16:24:12 -0500

    fix

--- server/config/config.example.js
+++
@@ -1,9 +0,0 @@
-//This file holds any configuration variables we may need
-//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
-
-module.exports = {
-    db: {
-        uri: 'mongodb+srv://datguy:2nnVif-A.x!*cQ9@doorsnacs-kwowd.mongodb.net/test?retryWrites=true&w=majority', 
-//place the URI of your mongo database here.
-    }
-};
