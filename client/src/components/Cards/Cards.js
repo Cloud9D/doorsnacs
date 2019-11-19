98834424311a3a3f6e81c4fa1ae32831d28efa40
Quinten Rivers <quintenrivers@Quintens-MacBook-Air.local>
2019-11-19 14:13:19 -0500

    updated cards/home layout

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -6,16 +6,32 @@ import { Card } from 'react-bootstrap';
 export default class Cards extends React.Component{
     render(){
         return(
-            <Card style={{ width: '25rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
+            <Card className="text-left">
+                <Card.Body>
+                    {/***************************************************
+                      * 
+                      * TODO LIST
+                      * fetch image from db
+                      * fetch description from db
+                      * fetch rating from db
+                      * fetch dynamic link for restaurant
+                      * fetch whether or not it is pickup/delivery only
+                      * 
+                      **************************************************/}
+                    <Card.Img
+                        variant="top"
+                        className="mb-3"
+                        src="https://www.tasteofhome.com/wp-content/uploads/2017/10/exps6498_MRR133247D07_30_5b_WEB-2.jpg"
+                    />
+                    <Card.Title>Restaurant/Dish Name</Card.Title>
+                    <Card.Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    </Card.Text>
+                    <a href="#">
+                        <Button variant="primary">Order Now</Button>
+                    </a>
+                </Card.Body>
+                <Card.Footer className="text-muted">4 out of 5</Card.Footer>
             </Card>
         );
     }
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -2,37 +2,36 @@ import React from 'react';
 import Cards from '../../components/Cards/Cards'
 import AddressDelivery from '../../components/AdddressDelivery/AddressDelivery';
 import './Home.css';
-
 export default class Home extends React.Component{
     render() {
         return (
             <div className="container">
-                <div className="jumbotron px-md-5 hero">
+                <div className="jumbotron px-5 hero">
                     <div className="row">
                         <h1 className="hero-text">Made at home.<br/>Just not yours.</h1>
                     </div>
                     <div className="row"><AddressDelivery /></div>
                 </div>
 
-                <div>
-                    <div className="row">
+                <div className="mb-5">
+                    <div className="row ml-1 mb-2">
                         <h3>Top Spots in Your Area</h3>
                     </div>
                     <div className="row">
-                        <div className="col"><Cards /></div>
-                        <div className="col"><Cards /></div>
-                        <div className="col"><Cards /></div>
+                        <div className="col mb-3"><Cards /></div>
+                        <div className="col mb-3"><Cards /></div>
+                        <div className="col mb-3"><Cards /></div>
                     </div>
                 </div>
 
-                <div className="my-md-5">
-                    <div className="row">
+                <div className="mb-5">
+                    <div className="row ml-1 mb-2">
                         <h3>Popular Items Near You</h3>
                     </div>
                     <div className="row">
-                        <div className="col"><Cards /></div>
-                        <div className="col"><Cards /></div>
-                        <div className="col"><Cards /></div>
+                        <div className="col mb-3"><Cards /></div>
+                        <div className="col mb-3"><Cards /></div>
+                        <div className="col mb-3"><Cards /></div>
                     </div>
                 </div>
             </div>
