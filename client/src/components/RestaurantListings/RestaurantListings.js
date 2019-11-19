002686af393530b23d8983bebb8dfc110ff8e1df
WittySkates <connordupuis760@gmail.com>
2019-11-19 03:28:13 -0500

    Added Profile Page

---
+++ client/src/components/RestaurantListings/RestaurantListings.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import GoogleSignIn from '../Google/GoogleSignIn'
+
+export default class RestaurantListings extends React.Component{
+    
+
+
+    render(){
+        return(
+            <div></div>
+        );
+    }
+} 
