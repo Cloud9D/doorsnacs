cfd88ed6b222578e40acba5ab03412d67a50f8a7
Quinten Rivers <quintendrivers@gmail.com>
2019-12-03 12:57:04 -0500

    updated profile page

--- client/src/views/Profile/Profile.js
+++ client/src/views/Profile/Profile.js
@@ -3,8 +3,12 @@
 import React, { Component } from 'react';
 import Image from 'react-bootstrap/Image';
 import Cards from '../../components/Cards/Cards';
-import ProfileHold from '../../assets/profile.png'
+import ProfileHold from '../../assets/profile.png';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import './Profile.css';
+
+import cart from '../../assets/icons/shopping_cart24px.svg';
 
 export default class Profile extends React.Component{
     constructor(props) {
@@ -12,7 +16,9 @@ export default class Profile extends React.Component{
         this.state = { 
             isAuthing : true,
             isSignedIn: false,
-            Name: ''
+            Name: '',
+            uri: '',
+            email: ''
         }
     }
 
@@ -21,10 +27,14 @@ export default class Profile extends React.Component{
         var googleUser = auth2.currentUser.get();
         
         if(googleUser.isSignedIn()){
+            let profile = googleUser.getBasicProfile();
+
             this.setState({
                 isSignedIn: googleUser.isSignedIn(),
-                Name: googleUser.getBasicProfile().getName(),
+                Name: profile.getName(),
                 isAuthing: false,
+                uri: profile.getImageUrl(),
+                email: profile.getEmail()
             });
         }
         else{
@@ -50,32 +60,59 @@ export default class Profile extends React.Component{
         if(!this.state.isAuthing){
             if(this.state.isSignedIn){
                 content = (
-                    <div className="ProfileTop">
-                        <div className="Profile-body">
-                            <div style={{backgroundColor:"#4b4a52", paddingTop:"50px", paddingBottom:"50px"}}>
-                                <div className="IMG">
-                                    <div style={{display:"inline-block", paddingRight:"25px"}}>
-                                        <Image src={ProfileHold} style={{width:"100px", height:"100px", backgroundColor:"Gray",}} roundedCircle />
-                                    </div>
-                                    <div style={{display:"inline-block", color:"whitesmoke"}}>
-                                        <h2>{this.state.Name}</h2>
-                                    </div>
-                                </div>
-
+                    <div className="container mt-3">
+                        <div className="row">
+                            <Image
+                                src={this.state.uri}
+                                roundedCircle
+                                className="col-2"
+                            />
+                            <div className="col my-auto">
+                                <h2 className="userName">{this.state.Name}</h2>
+                                <h4 className="userEmail">{this.state.email}</h4>
                             </div>
                         </div>
+
+                        <div className="mt-5">
+                            <div className="mt-3 row space-between">
+                            <h1>Your Cart Items <Image src={cart} className="cart-img" /></h1>
+                            </div>
+
+                            <div>
+                                {/* 
+                                    Has to be replaced with a function that iterates through
+                                    cart items and produces a <li></li> with the right data
+                                */}
+                                <ul>
+                                    <li className="mt-3 row space-between cart-item">
+                                        <p className="cart-item-product">Apples</p>
+                                        <p className="cart-item-price">$1.99</p>
+                                    </li>
+
+                                    <li className="row space-between cart-item">
+                                        <p className="cart-item-product">Apples</p>
+                                        <p className="cart-item-price">$1.99</p>
+                                    </li>
+                                </ul>
+
+                                <div className="mt-3 row space-between">
+                                    <h1>Total</h1>
+                                    <h1>$3.98</h1>
+                                </div>
+
+                                <ButtonToolbar className="space-between mt-1 d-flex flex-row-reverse">
+                                    <Button variant="primary" size="lg">Checkout</Button>
+                                    <Button variant="outline-danger mr-2" size="lg">Clear</Button>
+                                </ButtonToolbar>
+                            </div>
+                        </div>
+
                     </div>
                 )
             }
             else{
                 content = (
-                    <div className="ProfileTop">
-                        <div className="Profile-body">
-                            <div style={{color:"whitesmoke", textAlign:"center"}}>
-                                <h1 style={{paddingTop:"20%"}}>Please Sign In and Refresh the Page</h1>
-                            </div>
-                        </div>
-                    </div>
+                    <h1 className="ml-2 mt-2">Please sign-in and refresh your browser</h1>
                 )
             }
         }
