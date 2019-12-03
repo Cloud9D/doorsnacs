ca2b5e562d55a629758d70f8fe361f5294b745ab
WittySkates <connordupuis760@gmail.com>
2019-12-03 16:17:22 -0500

    dynamic home page

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import Cards from '../../components/Cards/Cards'
-import AddressDelivery from '../../components/AdddressDelivery/AddressDelivery';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
 import './Home.css';
 export default class Home extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            restaurantList: []
+        }
+    }
+
+    getApi = () =>{
+        fetch('http://localhost:5000/api/restaurant')
+            .then(response => response.json())
+            .then(response => {
+                this.setState({restaurantList: response})
+                console.log(this.state.restaurantList)
+
+            })
+    }
+    componentDidMount = () =>{
+        this.getApi();
+
+    }
+
+
     render() {
         return (
             <div className="container">
@@ -10,28 +33,36 @@ export default class Home extends React.Component{
                     <div className="row">
                         <h1 className="hero-text">Made at home.<br/>Just not yours.</h1>
                     </div>
-                    <div className="row"><AddressDelivery /></div>
                 </div>
 
                 <div className="mb-5">
                     <div className="row ml-1 mb-2">
                         <h3>Top Spots in Your Area</h3>
                     </div>
-                    <div className="row">
-                        <div className="col mb-3"><Cards /></div>
-                        <div className="col mb-3"><Cards /></div>
-                        <div className="col mb-3"><Cards /></div>
-                    </div>
-                </div>
-
-                <div className="mb-5">
-                    <div className="row ml-1 mb-2">
-                        <h3>Popular Items Near You</h3>
-                    </div>
-                    <div className="row">
-                        <div className="col mb-3"><Cards /></div>
-                        <div className="col mb-3"><Cards /></div>
-                        <div className="col mb-3"><Cards /></div>
+                    <div style={{paddingRight: "30px"}}>
+                    <div className="container-fluid">
+                        <div className="row" style={{paddingTop:"15px", paddingLeft:"15px"}}> {
+                            this.state.restaurantList.map(restaurant =>
+                                <div style={{paddingRight: "30px"}}>    
+                                    <Card className="text-left" style={{width:'300px'}}>
+                                        <Card.Body>
+                                            <Card.Img
+                                                variant="top"
+                                                className="mb-3"
+                                                src="https://cdn.pixabay.com/photo/2016/09/13/18/38/silverware-1667988__340.png"
+                                            />
+                                            <Card.Title>{restaurant.name}</Card.Title>
+                                            <Card.Text>{restaurant.description}</Card.Text>
+                                            <a href="#">
+                                                <Button variant="primary" onClick={() => {this.redirectClick(restaurant._id)}}>Check Me Out</Button>
+                                            </a>
+                                        </Card.Body>
+                                        <Card.Footer className="text-muted">{restaurant.rating}</Card.Footer>
+                                    </Card>
+                                </div>)
+                        }
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
